8cb87ed13fc14ed2019f3795d4051da851224d7f
Alex <88888alexandr@gmail.com>
2018-01-10 10:43:36 +0300

    step2 _ 01

--- src/compare.js
+++ src/compare.js
@@ -1,49 +1,47 @@
 
-import * as fs from 'fs';
+import fs from 'fs';
 
-const f = (file1 = '/home/alex/before.json', file2 = '/home/alex/after.json') => {
+const f = (file1, file2) => {
   const text1 = fs.readFileSync(file1, 'utf8');
   const text2 = fs.readFileSync(file2, 'utf8');
 
   const obj1 = JSON.parse(text1);
   const obj2 = JSON.parse(text2);
-  const objReturn = { gdfgdfg: 'dfgdfgdfg' };
 
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const key12 = keys1.concat(keys2);
 
-  const arrAll = key12.filter((item, pos) => key12.indexOf(item) === pos);
+  const arrAll = [...new Set(key12)];// key12.filter((item, pos) => key12.indexOf(item) === pos);
 
-  console.log(arrAll);
+  const objReturn = {};
 
-  // arrAll.map(currentValue)
+  arrAll.map((currentValue) => {
+    if ((typeof obj1[currentValue] !== 'undefined')
+      && (typeof obj2[currentValue] !== 'undefined')
+      && (obj1[currentValue] === obj2[currentValue])) {
+      objReturn[currentValue] = obj1[currentValue];
+    }
 
-  // console.log(obj2);
+    if ((typeof obj1[currentValue] !== 'undefined')
+      && (typeof obj2[currentValue] !== 'undefined')
+      && (obj1[currentValue] !== obj2[currentValue])) {
+      objReturn[`+ ${currentValue}`] = obj2[currentValue];
+      objReturn[`- ${currentValue}`] = obj1[currentValue];
+    }
 
-  // const diffObject = Object.keys(obj1).reduce((accumulator, currentValue) => {
-  //   console.log(currentValue);
-  // //   const newMap = map;
-  // //   if (obj1[k] !== obj2[k]) {
-  // //     newMap[k] = obj2[k];
-  // //   }
-  //    return 'newMap';
-  // }, '');
+    if ((typeof obj1[currentValue] !== 'undefined')
+      && (typeof obj2[currentValue] === 'undefined')) {
+      objReturn[`- ${currentValue}`] = obj1[currentValue];
+    }
 
+    if ((typeof obj2[currentValue] !== 'undefined')
+      && (typeof obj1[currentValue] === 'undefined')) {
+      objReturn[`+ ${currentValue}`] = obj2[currentValue];
+    }
 
-  // for (const key in obj1) {
-  //   if ({}.hasOwnProperty.call(obj1, key)) {
-  //     // 1. key and value are equal:
-  //     if (typeof obj2[key] !== 'undefined') {
-  //       if (obj1[key] === obj2[key]) {
-  //         objReturn.push({ key: obj1[key] });
-  //       }
-  //     }
-  //   }
-  // }
-
-
-  // console.log(diffObject);
+    return '';
+  });
 
   return objReturn;
 };
--- start.js
+++ start.js
@@ -5,8 +5,5 @@ import f from './src/compare';
 const file1 = '/home/alex/before.json';
 const file2 = '/home/alex/after.json';
 
-// const text1 = fs.readFileSync(file1, 'utf8');
-// const text2 = fs.readFileSync(file2, 'utf8');
-
 const objCompare = f(file1, file2);
 console.log(objCompare);
