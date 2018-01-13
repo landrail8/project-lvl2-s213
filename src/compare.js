f9003017bc2028984a6d8412809a937793c6ee63
MadGambler <88888alexandr@gmail.com>
2018-01-13 10:44:27 +0300

    step 02

--- src/compare.js
+++ src/compare.js
@@ -1,65 +1,77 @@
 
 import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
-import ini from 'ini';
+import _ from 'lodash';
+
+// import path from 'path';
+// import yaml from 'js-yaml';
+// import ini from 'ini';
 
 const f = (file1, file2) => {
   const text1 = fs.readFileSync(file1, 'utf8');
   const text2 = fs.readFileSync(file2, 'utf8');
 
-  let obj1 = {};
-  let obj2 = {};
+  // let obj1 = {};
+  // let obj2 = {};
+  //
+  // if (path.extname(file1) === '.json') {
+  const obj1 = JSON.parse(text1);
+  const obj2 = JSON.parse(text2);
+  // }
+  //
+  // if (path.extname(file1) === '.yaml') {
+  //   obj1 = yaml.safeLoad(text1);
+  //   obj2 = yaml.safeLoad(text2);
+  // }
+  //
+  // if (path.extname(file1) === '.ini') {
+  //   obj1 = ini.parse(text1);
+  //   obj2 = ini.parse(text2);
+  // }
 
-  if (path.extname(file1) === '.json') {
-    obj1 = JSON.parse(text1);
-    obj2 = JSON.parse(text2);
-  }
+  const key12 = _.union(_.keys(obj1), _.keys(obj2));
 
-  if (path.extname(file1) === '.yaml') {
-    obj1 = yaml.safeLoad(text1);
-    obj2 = yaml.safeLoad(text2);
-  }
-
-  if (path.extname(file1) === '.ini') {
-    obj1 = ini.parse(text1);
-    obj2 = ini.parse(text2);
-  }
-
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const key12 = keys1.concat(keys2);
-
-  const arrAll = [...new Set(key12)];
-
-  const objReturn = {};
-
-  arrAll.map((currentValue) => {
+  const objReturn = key12.reduce((acc, currentValue) => {
     if ((typeof obj1[currentValue] !== 'undefined')
       && (typeof obj2[currentValue] !== 'undefined')
       && (obj1[currentValue] === obj2[currentValue])) {
-      objReturn[`  ${currentValue}`] = obj1[currentValue];
+      const key = `  ${currentValue}`;
+      const newObj = {};
+      newObj[key] = obj1[currentValue];
+      return Object.assign(acc, newObj);
     }
 
     if ((typeof obj1[currentValue] !== 'undefined')
       && (typeof obj2[currentValue] !== 'undefined')
       && (obj1[currentValue] !== obj2[currentValue])) {
-      objReturn[`+ ${currentValue}`] = obj2[currentValue];
-      objReturn[`- ${currentValue}`] = obj1[currentValue];
+      const key1 = `+ ${currentValue}`;
+      const newObj1 = {};
+      newObj1[key1] = obj2[currentValue];
+
+      const key2 = `- ${currentValue}`;
+      const newObj2 = {};
+      newObj2[key2] = obj1[currentValue];
+
+      return Object.assign(acc, newObj1, newObj2);
     }
 
     if ((typeof obj1[currentValue] !== 'undefined')
       && (typeof obj2[currentValue] === 'undefined')) {
-      objReturn[`- ${currentValue}`] = obj1[currentValue];
+      const key = `- ${currentValue}`;
+      const newObj = {};
+      newObj[key] = obj1[currentValue];
+      return Object.assign(acc, newObj);
     }
 
     if ((typeof obj2[currentValue] !== 'undefined')
       && (typeof obj1[currentValue] === 'undefined')) {
-      objReturn[`+ ${currentValue}`] = obj2[currentValue];
+      const key = `+ ${currentValue}`;
+      const newObj = {};
+      newObj[key] = obj2[currentValue];
+      return Object.assign(acc, newObj);
     }
 
-    return '';
-  });
+    return acc;
+  }, {});
 
   return objReturn;
 };
